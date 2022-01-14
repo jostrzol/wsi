c9f3c051eac4cdb92df4e0e401b2553908786c12
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2022-01-14 16:07:01 +0100

    experiments

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 from typing import List
 import gym
 from q_learning import QLearningAgent
+from matplotlib import pyplot as plt
 import numpy as np
 
-EPISODES = 10000
-EPISODE_LOG_INTERVAL = 1000
+EPISODES = 100_000
+EPISODE_LOG_INTERVAL = 10_000
+PLOT_LOOKBEHIND = 10_000
+
+EPSILON_SCALING_FACTOR = 0.5
+EPSILON_MIN = 0.001
+
+MAX_TURNS = 200
 
 if __name__ == "__main__":
     env = gym.make('FrozenLake-v1')
 
     agent = QLearningAgent(
-        range(env.observation_space.n),
-        range(env.action_space.n),
-        initial_learning_rate=1,
-        leaerning_rate_scaling_factor=0.95,
-        discount=0.5
+        env.observation_space.n,
+        env.action_space.n,
+        discount=0.85,
+        learning_rate_initial=0.8,
+        epsilon_initial=1,
+        # epsilon_scaling_factor=0.99999,
+        # epsilon_min=0.001,
+        # temperature=0.8
     )
 
     rewards: List[float] = []
-    for i in range(EPISODES):
+    for i in range(1, EPISODES + 1):
         state = env.reset()
         is_done = False
-        while not is_done:
-            action = agent.decide(state)
+        reward = 0
+        for _ in range(MAX_TURNS):
+            # env.render()
+            action = agent.decide_epsilon(state)
             next_state, reward, is_done, info = env.step(action)
+            # if is_done != 0:
+            #     x = next_state % 4
+            #     y = next_state // 4
+            #     dst_y = 3
+            #     dst_x = 3
+            #     dist = abs(x - dst_x) + abs(y - dst_y)
+            #     reward = 1 / (dist + 1)
 
             agent.update_q(
                 state,
@@ -33,13 +52,25 @@ if __name__ == "__main__":
                 is_done
             )
             state = next_state
+            if is_done:
+                break
 
         rewards.append(reward)
+        agent.epsilon = max(
+            agent.epsilon * EPSILON_SCALING_FACTOR,
+            EPSILON_MIN
+        )
 
         if i % EPISODE_LOG_INTERVAL == 0:
+            to_print = rewards[-EPISODE_LOG_INTERVAL:]
             print(f'EPISODE {i}:')
-            print(f'\tMean reward:\t\t\t{np.mean(rewards)}')
-            print(f'\tReward standard deviation:\t{np.std(rewards)}')
-            rewards = []
+            print(f'\tMean reward:\t\t\t{np.mean(to_print)}')
+            print(f'\tReward standard deviation:\t{np.std(to_print)}')
 
     env.close()
+
+    x = np.arange(PLOT_LOOKBEHIND, len(rewards))
+    y = [np.mean(rewards[i - PLOT_LOOKBEHIND: i])
+         for i in range(PLOT_LOOKBEHIND, len(rewards))]
+    plt.plot(x, y)
+    plt.show()
--- q_learning.py
+++ q_learning.py
@@ -1,38 +1,79 @@
-from typing import Any, Dict, Iterable, Tuple
+from typing import Any, Dict, Iterable
+
 import numpy as np
+from random import random
 
 
 class QLearningAgent:
-    _q: Dict[Any, Dict[Any, float]]
-    learning_rate: float
-    leaerning_rate_scaling_factor: float
+    _q: np.ndarray
+    _n_actions: int
+    _n_states: int
     discount: float
 
-    def __init__(self,
-                 states: Iterable[Any],
-                 actions: Iterable[Any],
-                 initial_learning_rate: float,
-                 leaerning_rate_scaling_factor: float,
-                 discount: float):
-        self._q = {s: {a: 1 for a in actions} for s in states}
-        self.learning_rate = initial_learning_rate
-        self.leaerning_rate_scaling_factor = leaerning_rate_scaling_factor
-        self.discount = discount
+    learning_rate: float
+    # learning_rate_scaling_factor: float
+    # learning_rate_min: float
 
-    def decide(self, state: Any):
-        actions = list(self._q[state].keys())
-        weights = list(self._q[state].values())
-        probabilities = weights / np.sum(weights)
-        return np.random.choice(actions, size=1, p=probabilities)[0]
+    epsilon: float
+    # epsilon_scaling_factor: float
+    # epsilon_min: float
+
+    temperature: float
+
+    def __init__(self,
+                 n_states: int,
+                 n_actions: int,
+                 discount: float,
+                 learning_rate_initial: float,
+                 #  learning_rate_scaling_factor: float = 1,
+                 #  learning_rate_min: float = 0,
+                 epsilon_initial: float = 1,
+                 #  epsilon_scaling_factor: float = 0.99999,
+                 #  epsilon_min: float = 0.001,
+                 temperature: float = 0.5):
+        self._n_states = n_states
+        self._n_actions = n_actions
+        self._q = np.zeros((n_states, n_actions))
+        self.learning_rate = learning_rate_initial
+        # self.leaerning_rate_scaling_factor = learning_rate_scaling_factor
+        # self.learning_rate_min = learning_rate_min
+        self.discount = discount
+        self.epsilon = epsilon_initial
+        # self.epsilon_scaling_factor = epsilon_scaling_factor
+        # self.epsilon_min = epsilon_min
+        self.temperature = temperature
+
+    def decide_epsilon(self, state: int):
+        weights = self._q[state]
+        total_weights = weights.sum()
+
+        if random() < self.epsilon or total_weights == 0:
+            probabilities = None
+        else:
+            probabilities = weights / total_weights
+
+        # self.epsilon *= self.epsilon_scaling_factor
+        # self.epsilon_min = max(self.epsilon_min,
+        #                        self.epsilon)
+
+        return np.random.choice(
+            range(self._n_actions), size=1, p=probabilities)[0]
+
+    def decide_boltzmann(self, state: int):
+        weights = np.exp(self._q[state] / self.temperature)
+        probabilities = weights / weights.sum()
+
+        return np.random.choice(
+            range(self._n_actions), size=1, p=probabilities)[0]
 
     def update_q(self,
-                 state: Any,
-                 next_state: Any,
+                 state: int,
+                 next_state: int,
                  action: Any,
                  reward: float,
                  is_done: bool):
         if not is_done:
-            q_max_next_state = max(self._q[next_state].values())
+            q_max_next_state = self._q[next_state].max()
         else:
             q_max_next_state = 0
 
@@ -42,4 +83,6 @@ class QLearningAgent:
             self._q[state][action])
         self._q[state][action] += change
 
-        self.learning_rate *= self.leaerning_rate_scaling_factor
+        # self.learning_rate *= self.leaerning_rate_scaling_factor
+        # self.learning_rate_min = max(self.learning_rate_min,
+        #                              self.learning_rate)
